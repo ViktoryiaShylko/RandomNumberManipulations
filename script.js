415763b4a4f85e9253247c6e442316dc978a3484
ViktoryiaShylko <vika.shylko@mail.ru>
2022-07-18 15:08:33 +0300

    changes: js

--- script.js
+++ script.js
@@ -6,33 +6,33 @@ btn.addEventListener('click', function generate () {
     document.getElementById('average').innerHTML = "";
     document.getElementById('sum').innerHTML = "";
     document.getElementById('multiply').innerHTML = "";
+    
     let numbersArr = [];
 
-    for (let i = -10; i<=10; i++) {
-    numbersArr.push(i);
-    console.log(numbersArr);
+    function randomizer (min, max){
+    num = Math.round(min - 0.5 + Math.random() * (max - min + 1));
+    return num;
     }
-    for(cycle = 0;cycle<=10;cycle++){
-    numbersArr.splice(Math.random()*numbersArr.length, 1)[0];
-    }
-    document.getElementById('randomNumbers').innerHTML += numbersArr;
 
-let min = Math.min.apply(null, numbersArr);
+    for(cycle = 0;cycle<=10;cycle++)  {
+    randomizer(-10 , 10);
+    numbersArr.push(num);
+    } 
+    document.getElementById('randomNumbers').innerHTML += numbersArr;
+    
+
+
+let min = Math.min(...numbersArr);
 document.getElementById('min').innerHTML += min;
 
-let max = Math.max.apply(null, numbersArr);
+let max = Math.max(...numbersArr);
 document.getElementById('max').innerHTML += max;
 
-let sum = 0;
-for(let i = 0; i < numbersArr.length; i++){
-        sum += numbersArr[i];
-        }
+let sum = numbersArr.reduce((previousNumber, currentNumber) => previousNumber + currentNumber);
+
 document.getElementById('sum').innerHTML += sum;
 document.getElementById('average').innerHTML += sum/10;
 
-let multiply=1;
-for(let i = 0; i < numbersArr.length; i++){
-    multiply *= numbersArr[i];
-    }
+let multiply = numbersArr.reduce((previousNumber, currentNumber) => previousNumber * currentNumber, 1);
 document.getElementById('multiply').innerHTML += multiply;
 })
